b6267bd6850a96a93b2a62880ea7a71c877a3b6f
haohaojin <hao.jin@outlook.com>
2015-12-22 11:20:07 +0800

    ranking function implemented

--- getRankTest.py
+++ getRankTest.py
@@ -6,13 +6,18 @@ def getRank(url):
     try:
         html = urlopen(url)
     except HTTPError as e:
-        return None
+        return "e"
     try:
         bsObj = BeautifulSoup(html.read(), "html.parser")
-        title = bsObj.find_all("font", {"id":"collectsnum"}) # .parent.previous_sibling.get_text()
-        print(title[0].getText())
+        collectsnum = bsObj.find_all("font", {"id":"collectsnum"}) # .parent.previous_sibling.get_text()
+        title = bsObj.find_all("h1") # .parent.previous_sibling.get_text()
+        if collectsnum != None and len(collectsnum) > 0:
+            if int(collectsnum[0].getText()) > 100:
+                print(collectsnum[0].getText() + "|" + title[0].getText() + "|" + url)
     except AttributeError as e:
-        return None
+        return "e2"
 
-
-getRank("http://www.douguo.com/cookbook/1301535.html")
+for cookbookid in range(100000,10000000):
+    #print(cookbookid)
+    urlstring = "http://www.douguo.com/cookbook/" + str(cookbookid) + ".html"
+    getRank(urlstring)
