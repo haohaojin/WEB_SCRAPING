76d4b5a7949a7e384908ec2432addd85d39aab75
haohaojin <hao.jin@outlook.com>
2015-12-23 16:15:44 +0800

    ranking function implemented

--- scapetest.py
+++ scapetest.py
@@ -6,6 +6,8 @@ import csv
 import datetime
 import time
 
+# web scraping page 121
+
 def getBookmark(url):
     try:
         html = urlopen(url)
@@ -13,10 +15,11 @@ def getBookmark(url):
         return None
     try:
         bsObj = BeautifulSoup(html.read(), "html.parser")
-        title = bsObj.find_all("span", {"id":"collectsnum"}) # .parent.previous_sibling.get_text()
+        title = bsObj.find_all("span", {"id": "collectsnum"})  # .parent.previous_sibling.get_text()
         return title[0].getText()
     except AttributeError as e:
         return None
+
 
 def getVisit(url):
     try:
@@ -25,10 +28,11 @@ def getVisit(url):
         return None
     try:
         bsObj = BeautifulSoup(html.read(), "html.parser")
-        title = title = bsObj.find_all("span", {"id":"collectsnum"})[0].previous_sibling.previous_sibling.getText()
+        title = title = bsObj.find_all("span", {"id": "collectsnum"})[0].previous_sibling.previous_sibling.getText()
         return title
     except AttributeError as e:
         return None
+
 
 def getRecipe(url):
     try:
@@ -39,27 +43,31 @@ def getRecipe(url):
         bsObj = BeautifulSoup(html.read(), "html.parser")
         h3list = bsObj.find_all("h3")
         for recipe in h3list:
-            #if "圣诞" in recipe.getText() and "糖霜" in recipe.getText():
-            year = bsObj.find("span", {"class":"yearnum dblok"}).getText()[0:4]
-            month_temp = recipe.parent.parent.parent.find("span", {"class":"dblok pts"}).getText()
+            # if "圣诞" in recipe.getText() and "糖霜" in recipe.getText():
+            year = bsObj.find("span", {"class": "yearnum dblok"}).getText()[0:4]
+            month_temp = recipe.parent.parent.parent.find("span", {"class": "dblok pts"}).getText()
             if len(month_temp) == 3:
                 month = month_temp[0:2]
             else:
                 month = "0" + month_temp[0]
-            day = month_temp = recipe.parent.parent.parent.find("span", {"class":"tdate"}).getText()
+            day = month_temp = recipe.parent.parent.parent.find("span", {"class": "tdate"}).getText()
             recipe_url = recipe.find("a").attrs['href']
             created_date = datetime.date(int(year), int(month), int(day))
             diff = today - created_date
-            print(created_date.strftime('%Y-%m-%d') + "|" + str(diff.days) + "|" + getVisit(recipe_url) + "|" + getBookmark(recipe_url) + "|" + recipe.getText() + "|" + recipe.find("a").attrs['href'])
-            writer.writerow((created_date.strftime('%Y-%m-%d'),str(diff.days),getVisit(recipe_url),getBookmark(recipe.find("a").attrs['href']),recipe.getText(),recipe.find("a").attrs['href']))
+            print(created_date.strftime('%Y-%m-%d') + "|" + str(diff.days) + "|" + getVisit(
+                recipe_url) + "|" + getBookmark(recipe_url) + "|" + recipe.getText() + "|" + recipe.find("a").attrs[
+                      'href'])
+            writer.writerow((created_date.strftime('%Y-%m-%d'), str(diff.days), getVisit(recipe_url),
+                             getBookmark(recipe.find("a").attrs['href']), recipe.getText(),
+                             recipe.find("a").attrs['href']))
         title = bsObj.find_all(lambda tag: tag.getText() == '下一页')
         if (len(title) != 0):
-            #print(title)
+            # print(title)
             newpage = title[0].find("a").attrs['href']
-            #print(newpage)
-            newpage_decoded = newpage.replace("圣诞 糖霜","%E5%9C%A3%E8%AF%9E%20%E7%B3%96%E9%9C%9C")
-            #print(newpage_decoded)
-            time.sleep(randint(5,9))
+            # print(newpage)
+            newpage_decoded = newpage.replace("圣诞 糖霜", "%E5%9C%A3%E8%AF%9E%20%E7%B3%96%E9%9C%9C")
+            # print(newpage_decoded)
+            time.sleep(randint(5, 9))
             getRecipe(newpage_decoded)
     except AttributeError as e:
         return None
@@ -73,6 +81,6 @@ today = datetime.date.today()
 
 with open("C:/Users/hao.jin/Desktop/Python/test.csv", 'w', newline='') as csvFile:
     writer = csv.writer(csvFile)
-    writer.writerow(('Created Date','Elapsed Days','Visited','Bookmarked','Name','Link'))
+    writer.writerow(('Created Date', 'Elapsed Days', 'Visited', 'Bookmarked', 'Name', 'Link'))
     getRecipe("http://www.douguo.com/u/u30362766298239/recipe")
 csvFile.close()
