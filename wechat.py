6da9a4d05b489a312f7030bb48f30c616216b606
haohaojin <hao.jin@outlook.com>
2015-12-28 22:19:27 +0800

    fixed year issue

--- wechat.py
+++ wechat.py
@@ -1,9 +1,25 @@
 import xml.dom.minidom
 
-
 input_xml_string = "<xml><ToUserName><![CDATA[toUser]]></ToUserName><FromUserName><![CDATA[fromUser]]></FromUserName><CreateTime>1348831860</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[this is a test]]></Content><MsgId>1234567890123456</MsgId></xml>"
+
 
 def get_tagname():
     doc = xml.dom.minidom.parseString(input_xml_string)
-    for node in doc.getElementsByTagName("data"):
-        print (node, node.tagName, node.getAttribute("version"))
+    collection = doc.documentElement
+    toUserName = collection.getElementsByTagName("ToUserName")[0].childNodes[0].data
+    fromUserName = collection.getElementsByTagName("FromUserName")[0].childNodes[0].data
+    createTime = collection.getElementsByTagName("CreateTime")[0].childNodes[0].data
+    msgType = collection.getElementsByTagName("MsgType")[0].childNodes[0].data
+    content = collection.getElementsByTagName("Content")[0].childNodes[0].data
+    msgId = collection.getElementsByTagName("MsgId")[0].childNodes[0].data
+
+
+    print(toUserName)
+    print(fromUserName)
+    print(createTime)
+    print(msgType)
+    print(content)
+    print(msgId)
+
+
+get_tagname()
