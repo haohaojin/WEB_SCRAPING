d8fbd7010d636fb1f8c53a2a7faaa7e5313cd8ff
haohaojin <hao.jin@outlook.com>
2016-01-05 15:34:36 +0800

    finalized mysql insertion

--- recipe_mysql.py
+++ recipe_mysql.py
@@ -1,7 +1,7 @@
 import pymysql
 
 conn = pymysql.connect(host='127.0.0.1',  # unix_socket='/tmp/mysql.sock',
-                       user='JINH', passwd='initial1', db='mysql', charset='utf8')
+                       user='read', passwd='read', db='mysql', charset='utf8')
 cur = conn.cursor()
 cur.execute("USE breadmum_recipe")
 
